f220c3efe5a22fae6ae88c2eea2b8920870abfed
MarkBaber <mark.baber@cern.ch>
2015-08-11 13:37:50 +0200

    More readable

--- HLTriggerOffline/SUSYBSM/src/SUSY_HLT_alphaT.cc
+++ HLTriggerOffline/SUSYBSM/src/SUSY_HLT_alphaT.cc
@@ -12,21 +12,21 @@ SUSY_HLT_alphaT::SUSY_HLT_alphaT(const edm::ParameterSet& ps)
 {
   edm::LogInfo("SUSY_HLT_alphaT") << "Constructor SUSY_HLT_alphaT::SUSY_HLT_alphaT " << std::endl;
   // Get parameters from configuration file
-  theTrigSummary_ = consumes<trigger::TriggerEvent>(ps.getParameter<edm::InputTag>("trigSummary"));
-  thePfJetCollection_ = consumes<reco::PFJetCollection>(ps.getParameter<edm::InputTag>("pfJetCollection"));
+  theTrigSummary_       = consumes<trigger::TriggerEvent>(ps.getParameter<edm::InputTag>("trigSummary"));
+  triggerResults_       = consumes<edm::TriggerResults>(ps.getParameter<edm::InputTag>("TriggerResults"));
   theCaloJetCollection_ = consumes<reco::CaloJetCollection>(ps.getParameter<edm::InputTag>("caloJetCollection"));
-  triggerResults_ = consumes<edm::TriggerResults>(ps.getParameter<edm::InputTag>("TriggerResults"));
-  HLTProcess_ = ps.getParameter<std::string>("HLTProcess");
-  triggerPath_ = ps.getParameter<std::string>("TriggerPath");
+  thePfJetCollection_   = consumes<reco::PFJetCollection>(ps.getParameter<edm::InputTag>("pfJetCollection"));
+  HLTProcess_           = ps.getParameter<std::string>("HLTProcess");
+  triggerPath_          = ps.getParameter<std::string>("TriggerPath");
   triggerPathAuxiliaryForHadronic_ = ps.getParameter<std::string>("TriggerPathAuxiliaryForHadronic");
-  triggerFilter_ = ps.getParameter<edm::InputTag>("TriggerFilter");
-  triggerPreFilter_ = ps.getParameter<edm::InputTag>("TriggerPreFilter");
-  ptThrJet_ = ps.getUntrackedParameter<double>("PtThrJet");
-  etaThrJet_ = ps.getUntrackedParameter<double>("EtaThrJet");
-  caloAlphaTThrTurnon_ = ps.getUntrackedParameter<double>("caloAlphaTThrTurnon");
-  caloHtThrTurnon_ = ps.getUntrackedParameter<double>("caloHtThrTurnon");
-  pfAlphaTThrTurnon_ = ps.getUntrackedParameter<double>("pfAlphaTThrTurnon");
-  pfHtThrTurnon_ = ps.getUntrackedParameter<double>("pfHtThrTurnon");
+  triggerFilter_        = ps.getParameter<edm::InputTag>("TriggerFilter");
+  triggerPreFilter_     = ps.getParameter<edm::InputTag>("TriggerPreFilter");
+  ptThrJet_             = ps.getUntrackedParameter<double>("PtThrJet");
+  etaThrJet_            = ps.getUntrackedParameter<double>("EtaThrJet");
+  caloHtThrTurnon_      = ps.getUntrackedParameter<double>("caloHtThrTurnon");
+  caloAlphaTThrTurnon_  = ps.getUntrackedParameter<double>("caloAlphaTThrTurnon");
+  pfHtThrTurnon_        = ps.getUntrackedParameter<double>("pfHtThrTurnon");
+  pfAlphaTThrTurnon_    = ps.getUntrackedParameter<double>("pfAlphaTThrTurnon");
 }
 
 SUSY_HLT_alphaT::~SUSY_HLT_alphaT()
@@ -160,15 +160,15 @@ void SUSY_HLT_alphaT::analyze(edm::Event const& e, edm::EventSetup const& eSetup
   //Fill the alphaT and HT histograms
   if(hltPfJets.size()>0){
       double hltPfAlphaT = AlphaT(hltPfJets,true).value();
-      h_triggerPfAlphaT->Fill(hltPfAlphaT);
-      h_triggerPfHt->Fill(hltPfHt);
+      h_triggerPfAlphaT            ->Fill(hltPfAlphaT);
+      h_triggerPfHt                ->Fill(hltPfHt);
       h_triggerPfAlphaT_triggerPfHt->Fill(hltPfHt, hltPfAlphaT);
   }
 
   if(hltCaloJets.size()>0){
       double hltCaloAlphaT = AlphaT(hltCaloJets,true).value();
-      h_triggerCaloAlphaT->Fill(hltCaloAlphaT);
-      h_triggerCaloHt->Fill(hltCaloHt);
+      h_triggerCaloAlphaT              ->Fill(hltCaloAlphaT);
+      h_triggerCaloHt                  ->Fill(hltCaloHt);
       h_triggerCaloAlphaT_triggerCaloHt->Fill(hltCaloHt, hltCaloAlphaT);
   }
 
@@ -207,21 +207,19 @@ void SUSY_HLT_alphaT::analyze(edm::Event const& e, edm::EventSetup const& eSetup
 
       //AlphaT aT = AlphaT(jets);
       double caloAlphaT = AlphaT(caloJets).value();
-      double pfAlphaT = AlphaT(pfJets).value();
+      double pfAlphaT   = AlphaT(pfJets).value();
 
       //Fill the turnons
       if(hasFired) {
-        if(caloHT>caloHtThrTurnon_) h_caloAlphaTTurnOn_num-> Fill(caloAlphaT);
-        if(caloAlphaT>caloAlphaTThrTurnon_) h_caloHtTurnOn_num-> Fill(caloHT);
-
-        if(pfHT>pfHtThrTurnon_) h_pfAlphaTTurnOn_num-> Fill(pfAlphaT);
-        if(pfAlphaT>pfAlphaTThrTurnon_) h_pfHtTurnOn_num-> Fill(pfHT);
+        if (caloHT     > caloHtThrTurnon_)     h_caloAlphaTTurnOn_num->Fill(caloAlphaT);
+        if (caloAlphaT > caloAlphaTThrTurnon_) h_caloHtTurnOn_num    ->Fill(caloHT);
+        if (pfHT       > pfHtThrTurnon_)       h_pfAlphaTTurnOn_num  ->Fill(pfAlphaT);
+        if (pfAlphaT   > pfAlphaTThrTurnon_)   h_pfHtTurnOn_num      ->Fill(pfHT);
       } 
-      if(caloHT>caloHtThrTurnon_) h_caloAlphaTTurnOn_den-> Fill(caloAlphaT);
-      if(caloAlphaT>caloAlphaTThrTurnon_) h_caloHtTurnOn_den-> Fill(caloHT);
-
-      if(pfHT>pfHtThrTurnon_) h_pfAlphaTTurnOn_den-> Fill(pfAlphaT);
-      if(pfAlphaT>pfAlphaTThrTurnon_) h_pfHtTurnOn_den-> Fill(pfHT);
+      if (caloHT     > caloHtThrTurnon_)     h_caloAlphaTTurnOn_den->Fill(caloAlphaT);
+      if (caloAlphaT > caloAlphaTThrTurnon_) h_caloHtTurnOn_den    ->Fill(caloHT);
+      if (pfHT       > pfHtThrTurnon_)       h_pfAlphaTTurnOn_den  ->Fill(pfAlphaT);
+      if (pfAlphaT   > pfAlphaTThrTurnon_)   h_pfHtTurnOn_den      ->Fill(pfHT);
   }
 }
 
@@ -245,24 +243,24 @@ void SUSY_HLT_alphaT::bookHistos(DQMStore::IBooker & ibooker_)
   //offline quantities
 
   //online quantities 
-  h_triggerCaloHt = ibooker_.book1D("triggerCaloHt", "Trigger Calo Ht; HT (GeV)", 60, 0.0, 1500.0);
-  h_triggerCaloAlphaT = ibooker_.book1D("triggerCaloAlphaT", "Trigger Calo AlphaT; AlphaT", 80, 0., 1.0);
+  h_triggerCaloHt                   = ibooker_.book1D("triggerCaloHt",                  "Trigger Calo Ht; HT (GeV)",   60, 0.0, 1500.0);
+  h_triggerCaloAlphaT               = ibooker_.book1D("triggerCaloAlphaT",              "Trigger Calo AlphaT; AlphaT", 80, 0., 1.0);
   h_triggerCaloAlphaT_triggerCaloHt = ibooker_.book2D("triggerCaloAlphaT_triggerCaloHt","Trigger Calo HT vs Trigger Calo AlphaT; HT (GeV); AlphaT", 60,0.0,1500.,80,0.,1.0);
-  h_triggerPfHt = ibooker_.book1D("triggerPfHt", "Trigger PF Ht; HT (GeV)", 60, 0.0, 1500.0);
-  h_triggerPfAlphaT = ibooker_.book1D("triggerPfAlphaT", "Trigger PF AlphaT; AlphaT", 80, 0., 1.0);
-  h_triggerPfAlphaT_triggerPfHt = ibooker_.book2D("triggerPfAlphaT_triggerPfHt","Trigger PF HT vs Trigger PF AlphaT; HT (GeV); AlphaT", 60,0.0,1500.,80,0.,1.0);
+  h_triggerPfHt                     = ibooker_.book1D("triggerPfHt",                "Trigger PF Ht; HT (GeV)",   60, 0.0, 1500.0);
+  h_triggerPfAlphaT                 = ibooker_.book1D("triggerPfAlphaT",            "Trigger PF AlphaT; AlphaT", 80, 0., 1.0);
+  h_triggerPfAlphaT_triggerPfHt     = ibooker_.book2D("triggerPfAlphaT_triggerPfHt","Trigger PF HT vs Trigger PF AlphaT; HT (GeV); AlphaT", 60,0.0,1500.,80,0.,1.0);
 
 
   //num and den hists to be divided in harvesting step to make turn on curves
-  h_caloAlphaTTurnOn_num = ibooker_.book1D("caloAlphaTTurnOn_num", "Calo AlphaT Turn On Numerator; AlphaT", 40, 0.0, 1.0 );
+  h_caloAlphaTTurnOn_num = ibooker_.book1D("caloAlphaTTurnOn_num", "Calo AlphaT Turn On Numerator; AlphaT",  40, 0.0, 1.0 );
   h_caloAlphaTTurnOn_den = ibooker_.book1D("caloAlphaTTurnOn_den", "Calo AlphaT Turn OnDenominator; AlphaT", 40, 0.0, 1.0 );
-  h_caloHtTurnOn_num = ibooker_.book1D("caloHtTurnOn_num", "Calo HT Turn On Numerator; HT (GeV)", 30, 0.0, 1500.0 );
-  h_caloHtTurnOn_den = ibooker_.book1D("caloHtTurnOn_den", "Calo HT Turn On Denominator; HT (GeV)", 30, 0.0, 1500.0 );
+  h_caloHtTurnOn_num     = ibooker_.book1D("caloHtTurnOn_num",     "Calo HT Turn On Numerator; HT (GeV)",    30, 0.0, 1500.0 );
+  h_caloHtTurnOn_den     = ibooker_.book1D("caloHtTurnOn_den",     "Calo HT Turn On Denominator; HT (GeV)",  30, 0.0, 1500.0 );
 
-  h_pfAlphaTTurnOn_num = ibooker_.book1D("pfAlphaTTurnOn_num", "PF AlphaT Turn On Numerator; AlphaT", 40, 0.0, 1.0 );
+  h_pfAlphaTTurnOn_num = ibooker_.book1D("pfAlphaTTurnOn_num", "PF AlphaT Turn On Numerator; AlphaT",  40, 0.0, 1.0 );
   h_pfAlphaTTurnOn_den = ibooker_.book1D("pfAlphaTTurnOn_den", "PF AlphaT Turn OnDenominator; AlphaT", 40, 0.0, 1.0 );
-  h_pfHtTurnOn_num = ibooker_.book1D("pfHtTurnOn_num", "PF HT Turn On Numerator; HT (GeV)", 30, 0.0, 1500.0 );
-  h_pfHtTurnOn_den = ibooker_.book1D("pfHtTurnOn_den", "PF HT Turn On Denominator; HT (GeV)", 30, 0.0, 1500.0 );
+  h_pfHtTurnOn_num     = ibooker_.book1D("pfHtTurnOn_num",     "PF HT Turn On Numerator; HT (GeV)",    30, 0.0, 1500.0 );
+  h_pfHtTurnOn_den     = ibooker_.book1D("pfHtTurnOn_den",     "PF HT Turn On Denominator; HT (GeV)",  30, 0.0, 1500.0 );
 
   ibooker_.cd();
 }
