f02699485e5362f5b2900f192712c126c86ba484
MarkBaber <mark.baber@cern.ch>
2015-09-29 22:07:37 +0200

    Enable only alphaT postprocessing

--- HLTriggerOffline/SUSYBSM/python/SUSYBSM_postProcessor_cff.py
+++ HLTriggerOffline/SUSYBSM/python/SUSYBSM_postProcessor_cff.py
@@ -34,42 +34,42 @@ from HLTriggerOffline.SUSYBSM.SUSYBSM_HLT_Electron_BJet_cff import *
 from HLTriggerOffline.SUSYBSM.SUSYBSM_alphaT_cff import *
 
 
-SusyExoPostVal = cms.Sequence(SUSY_HLT_HT_MET_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_POSTPROCESSING +
-                              SUSY_HLT_InclusiveMET_POSTPROCESSING +
-                              SUSY_HLT_InclusiveMET_NoNoiseCleaning_POSTPROCESSING +
-                              SUSY_HLT_InclusiveMET_HBHECleaned_POSTPROCESSING +
-                              SUSY_HLT_InclusiveMET_JetIdCleaned_POSTPROCESSING +
-                              SUSY_HLT_MET_BTAG_POSTPROCESSING + 
-                              SUSY_HLT_MET_MUON_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux200_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux250_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux300_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux350_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux400_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux475_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux600_POSTPROCESSING +
-                              SUSY_HLT_InclusiveHT_aux800_POSTPROCESSING +
-                              SUSY_HLT_MET_MUON_ER_POSTPROCESSING +
-                              SUSY_HLT_MET_HT_MUON_POSTPROCESSING +
-                              SUSY_HLT_MET_HT_MUON_ER_POSTPROCESSING +
-                              SUSY_HLT_MET_HT_MUON_BTAG_POSTPROCESSING + 
-                              SUSY_HLT_Razor_PostVal_POSTPROCESSING + 
-                              SUSY_HLT_CaloHT_POSTPROCESSING + 
-                              SUSY_HLT_PhotonHT_POSTPROCESSING +                             
-                              SUSY_HLT_HT_DoubleMuon_POSTPROCESSING +
-                              SUSY_HLT_HT_DoubleEle_POSTPROCESSING +
-                              SUSY_HLT_HT_MuEle_POSTPROCESSING +
-                              SUSY_HLT_Muon_BJet_POSTPROCESSING +
-                              SUSY_HLT_Electron_BJet_POSTPROCESSING +
-                              SUSY_HLT_Mu_HT_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Mu_HT_MET_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Mu_HT_BTag_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Mu_HT_Control_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Ele_HT_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Ele_HT_MET_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Ele_HT_BTag_SingleLepton_POSTPROCESSING +
-                              SUSY_HLT_Ele_HT_Control_SingleLepton_POSTPROCESSING+
+SusyExoPostVal = cms.Sequence(# SUSY_HLT_HT_MET_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveMET_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveMET_NoNoiseCleaning_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveMET_HBHECleaned_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveMET_JetIdCleaned_POSTPROCESSING +
+                              # SUSY_HLT_MET_BTAG_POSTPROCESSING + 
+                              # SUSY_HLT_MET_MUON_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux200_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux250_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux300_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux350_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux400_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux475_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux600_POSTPROCESSING +
+                              # SUSY_HLT_InclusiveHT_aux800_POSTPROCESSING +
+                              # SUSY_HLT_MET_MUON_ER_POSTPROCESSING +
+                              # SUSY_HLT_MET_HT_MUON_POSTPROCESSING +
+                              # SUSY_HLT_MET_HT_MUON_ER_POSTPROCESSING +
+                              # SUSY_HLT_MET_HT_MUON_BTAG_POSTPROCESSING + 
+                              # SUSY_HLT_Razor_PostVal_POSTPROCESSING + 
+                              # SUSY_HLT_CaloHT_POSTPROCESSING + 
+                              # SUSY_HLT_PhotonHT_POSTPROCESSING +                             
+                              # SUSY_HLT_HT_DoubleMuon_POSTPROCESSING +
+                              # SUSY_HLT_HT_DoubleEle_POSTPROCESSING +
+                              # SUSY_HLT_HT_MuEle_POSTPROCESSING +
+                              # SUSY_HLT_Muon_BJet_POSTPROCESSING +
+                              # SUSY_HLT_Electron_BJet_POSTPROCESSING +
+                              # SUSY_HLT_Mu_HT_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Mu_HT_MET_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Mu_HT_BTag_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Mu_HT_Control_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Ele_HT_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Ele_HT_MET_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Ele_HT_BTag_SingleLepton_POSTPROCESSING +
+                              # SUSY_HLT_Ele_HT_Control_SingleLepton_POSTPROCESSING+
                               SUSY_HLT_alphaT_POSTPROCESSING)
 
 SusyExoPostVal_fastsim = cms.Sequence(SUSY_HLT_HT_MET_FASTSIM_POSTPROCESSING +
